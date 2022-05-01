a934b00de389d0b28292966d79647e2760f223a0
Shatha Alrayyani <shatha.alrayyani223@gmail.com>
2022-05-01 06:33:42 +0300

    task12

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
 `use strict`
 
- let port = 3000;
- let express = require ("express");
+ let PORT = 3002;
+ let express = require("express");
 // creat express app
  let app = express();
 
- let movieData = require("./data.json");
+ let movieData = require("./Movie data/data.json");
  
  
- app.listen(port,() => {
-    console.log(`Hello on port ${port}`);
+ app.listen(PORT,() => {
+    console.log(`Hello on port ${PORT}`);
 });
- 
+
  app.get("/homePage", handlehomePage);
  app.get("/favorite", favoriteHandler);
+ app.get("/trending",trendingHandler);
 
+ function trendingHandler(req,res){
+     let movieInfo = new Movie(
+         movieData.id,
+         movieData.title,
+         movieData.release_date,
+         movieData.poster_path,
+         movieData.overview,
+         )
+         
+     res.json(movieInfo)
+    }
+
+ function handlehomePage(req, res) {
+     let newMovie = new Movie(
+         movieData.title,
+         movieData.poster_path,
+         movieData.overview,
+         )
+
+     res.json(newMovie);
+     }
+ 
+
+function Movie (title, poster_path,overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date ;
+    this.poster_path = poster_path;
+    this.overview = overview;
+} 
  
  function favoriteHandler (req, res) {
     res.send("Welcome to Favorite Page");
 }
-
- 
-  function Movie (title, poster_path,overview) {
-    this.title = title;
-    this.poster_path = poster_path;
-    this.overview = overview;  
-}
-
-
- function handlehomePage(req, res) {
-     let movie1 = [];
-     
-      movieData.data.forEach(element => {
- 
-         let newMovie = new Movie(
-             element.title,
-             element.poster_path,
-             element.overview,
-         );
-         movie1.push(newMovie);
-     })
-     console.log(movie1)
- 
-     res.json(movie1);
- 
- }
- 
-
-
-
